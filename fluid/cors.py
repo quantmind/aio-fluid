88a3b245b8528f4037d894a23f4ac2a336593d3f
Luca Sbardella <luca@quantmind.com>
2020-10-16 10:47:36 +0100

    Add cors

---
+++ fluid/cors.py
@@ -0,0 +1,23 @@
+import aiohttp_cors
+from aiohttp.web import Application
+
+
+def setup(app: Application):
+    # Configure default CORS settings.
+    cors = aiohttp_cors.setup(
+        app,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+        },
+    )
+
+    # Configure CORS on all routes.
+    for route in list(app.router.routes()):
+        try:
+            cors.add(route, webview=True)
+        except ValueError:
+            pass
