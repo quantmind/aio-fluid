6c7f7d6124ef8720781c2c04a9f46bfb2e5ed757
Luca <luca@b2c2.com>
2021-04-24 16:07:14 +0100

    add streaming batch_select

--- fluid/db.py
+++ fluid/db.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
+from typing import Iterator, List
 
 from aiohttp import web
 from openapi import json
 from openapi.data.validate import ValidationErrors
 from openapi.db import CrudDB
+from openapi.types import Record
+from sqlalchemy import Table
 
 
 class ExpectedOneOnly(RuntimeError):
@@ -20,3 +23,17 @@ class DbTools:
         raise web.HTTPUnprocessableEntity(
             body=json.dumps(data), content_type="application/json"
         )
+
+
+async def batch_select(
+    db: CrudDB,
+    table: Table,
+    *,
+    batch_size: int = 50,
+    **filters,
+) -> Iterator[List[Record]]:
+    query = db.get_query(table, table.select(), params=filters)
+    async with db.transaction() as conn:
+        result = await conn.stream(query)
+        async for rows in result.partiions(batch_size):
+            yield rows
